969cffada0422d3315f376d3d9facaba6833db58
leez4rd <101297800+leez4rd@users.noreply.github.com>
2022-05-24 16:18:00 -0400

    Slightly improved table script
    transposing table, putting all necessary data processing into functions so it is more generalizable and debuggable

--- making_table.py
+++ making_table.py
@@ -1,14 +1,8 @@
 import numpy as np
 import pandas as pd 
 
-path = 'https://github.com/erinwall/songpreferences/raw/main/final_data_summary.csv'
-df = pd.read_csv(path)
 
-by_bird_by_date = df.groupby(['bird_ID', 'date'])['bird_played'].apply(list).reset_index()
-
-all_tests = ['con vs het', 'fam vs unfam', 'familiar FD vs UD', 'unfamiliar FD vs UD - bkor',   'unfamiliar FD vs UD - blk17',   'unfamiliar FD vs UD - yelred',   'unfamiliar FD vs UD - yelgrn',  'pair2', 'pair3', 'pair4', 'tutor vs mate', 'pair1', 'pair5', 'misfit']
-
-
+# reference lists 
 listA = ['g32b89_UD', 'pi183y24_UD', 'r16y36', 'bl43p196_UD']
 listB = ['bkor_UD', 'blk17_UD', 'bl11pu3_UD', 'bl4b13_UD']
 listC = ['bl21pu11_FD', 'bl84gr17', 'bl80bk11_FD', 'bl31pi51_FD', 'bl74gy24_FD', 'bl71bl60_FD', 'bl63bk93_FD', 'bl87re02_FD', 'bl33ye35_FD', 'bl90pi15_FD', 'bl111gr09_FD', 'bl87pu80_FD', 'bl62or12_FD', 'bl49gy31_FD', 'bl61or11_FD', 'bl44pi74_FD', 'bl90or19_FD', 'bl43pi73_FD', 'bl58gr33_FD', 'bl100pu80_FD', 'bl34pu94_FD', 'bl86pi11_FD', 'bl65wh75_FD', 'bl14y6_FD', 'bl11pu3_FD', 'bl87bk3_FD', 'bl4b13_FD', 'pu55bl08_FD', 'bl17pi39_FD']
@@ -27,10 +21,12 @@ listO = ['bl32gy22_UD', 'bl17bl57_UD', 'bl17bl57_FD']
 listP = ['pu21pu22_UD', 'pu17pu18_UD']
 listQ = ['b85w66', 'pu54bl21']
 listU = ['p3y3', 'pu46w46']
+all_tests = ['con vs het', 'fam vs unfam', 'familiar FD vs UD', 'unfamiliar FD vs UD - bkor',   'unfamiliar FD vs UD - blk17',   'unfamiliar FD vs UD - yelred',   'unfamiliar FD vs UD - yelgrn',  'pair2', 'pair3', 'pair4', 'tutor vs mate', 'pair1', 'pair5', 'misfit']
 
+
+# data processing functions
 def determine_test_type(song_pair):
   test_type = None
-
   # note: probably a better way to do this, but too late now
 
   if ((song_pair[0] in listA and song_pair[1] in listB) or (song_pair[1] in listA and song_pair[0] in listB)):
@@ -64,60 +60,60 @@ def determine_test_type(song_pair):
   return test_type
 
 
-test_types = []
-for i in range(len(by_bird_by_date)):
 
-  current_list = by_bird_by_date['bird_played'][i]
-  if len(current_list) == 2:
+def make_test_lists(data):
+  test_types = []
+  for i in range(len(data)):
 
-    y = [current_list[0]] + [current_list[1]]
-    test_type = determine_test_type(y)
-    test_types += [test_type] 
+    current_list = data['bird_played'][i]
+    if len(current_list) == 2:
 
-
-  elif len(current_list) == 3:
-
-    sublist = []
-    y = [current_list[0]] + [current_list[1]]
-    test_type = determine_test_type(y)
-
-    sublist += [test_type] 
-    y = [current_list[1]] + [current_list[2]]
-    test_type = determine_test_type(y)
-
-    sublist += [test_type] 
-    y = [current_list[0]] + [current_list[2]]
-    test_type = determine_test_type(y)
-
-    sublist += [test_type] 
-    test_types += [sublist]
-
-  elif len(current_list) == 4:
-    sublist = []
-
-    y = [current_list[0]] + [current_list[1]]
-    test_type = determine_test_type(y)
-
-    sublist += [test_type] 
-    y = [current_list[2]] + [current_list[3]]
-    test_type = determine_test_type(y)
-
-    sublist += [test_type] 
-    test_types += [sublist]
-
-  else:
-    test_types += [None]
-    pass 
+      y = [current_list[0]] + [current_list[1]]
+      test_type = determine_test_type(y)
+      test_types += [test_type] 
 
 
-by_bird_by_date['test type'] = test_types
+    elif len(current_list) == 3:
 
+      sublist = []
+      y = [current_list[0]] + [current_list[1]]
+      test_type = determine_test_type(y)
+
+      sublist += [test_type] 
+      y = [current_list[1]] + [current_list[2]]
+      test_type = determine_test_type(y)
+
+      sublist += [test_type] 
+      y = [current_list[0]] + [current_list[2]]
+      test_type = determine_test_type(y)
+
+      sublist += [test_type] 
+      test_types += [sublist]
+
+    elif len(current_list) == 4:
+      sublist = []
+
+      y = [current_list[0]] + [current_list[1]]
+      test_type = determine_test_type(y)
+
+      sublist += [test_type] 
+      y = [current_list[2]] + [current_list[3]]
+      test_type = determine_test_type(y)
+
+      sublist += [test_type] 
+      test_types += [sublist]
+
+    else:
+      test_types += [None]
+      pass 
+  data['test type'] = test_types
+  return data
 
 
 
 #-----------------------      New Code    ------------------------------------#
 # --------------------------------------------------------------------------- # 
-
+'''
 test_type_freq = {} # keeps track of number of tests for each bird 
 for test_type in all_tests:
   test_type_freq[test_type] = 0
@@ -156,8 +152,81 @@ for i in by_bird_by_date.index:
     current_bird = by_bird_by_date['bird_ID'][i]
 
 # this gives something like a table, but the numbers are suspect so will need to debug 
+
 test_counts  = pd.DataFrame.from_dict(test_counts)
+test_counts = test_counts.transpose()
+# print("Test types\t\t\t\t\t\t\t\t\t Bird IDs\n")
 
-print("Test types\t\t\t\t\t\t\t\t\t Bird IDs\n")
+# print(test_counts)
 
-print(test_counts)
+# test_counts.to_csv('csvification.csv')
+'''
+
+
+
+def make_testcounts_table(data, df):
+  test_type_freq = {} # keeps track of number of tests for each bird -- each bird will have a different value for this 
+
+  # initialize dictionary entries to 0
+  all_tests_set = set(all_tests)
+  for test_type in all_tests_set:
+    test_type_freq[test_type] = 0
+
+  test_counts = {} # the final table as a nested dictionary 
+  
+  for ID in df['bird_ID'].unique(): # might want to avoid referencing df here
+    test_counts[ID] = 0
+
+  
+  current_bird = None
+  for i in data.index:
+    bird = data['bird_ID'][i]
+
+    # check if we are on a different bird yet
+    if bird == current_bird:  
+      # if we are not, keep incrementing the counts for each test type 
+      tests_for_day = data['test type'][i]
+      if tests_for_day != None:
+        if type(tests_for_day) == list:
+          for element in tests_for_day:
+            test_type_freq[element] += 1
+        else:
+          test_type_freq[tests_for_day] += 1
+    # if we are, we need to reset the dictionary and store the values 
+    else:
+      # store values from current dictionary 
+      test_counts[current_bird] = test_type_freq
+      # make new dictionary 
+
+      test_type_freq = {} # keeps track of number of tests for each bird 
+      for test_type in all_tests_set:
+        test_type_freq[test_type] = 0
+      # set current bird to this bird 
+      current_bird = data['bird_ID'][i]
+
+  # this gives something like a table, but the numbers are suspect so will need to debug 
+
+  test_counts  = pd.DataFrame.from_dict(test_counts)
+  test_counts = test_counts.transpose()
+  return test_counts
+
+
+def main():
+  path = 'https://github.com/erinwall/songpreferences/raw/main/final_data_summary.csv'
+  df = pd.read_csv(path)
+
+
+  by_bird_by_date = df.groupby(['bird_ID', 'date'])['bird_played'].apply(list).reset_index()
+
+  print(by_bird_by_date)
+  by_bird_by_date = make_test_lists(by_bird_by_date)
+  print(by_bird_by_date)
+  final_table = make_testcounts_table(by_bird_by_date, df) # this function takes the output of make_test_lists and the original data
+  print(final_table)
+  final_table.to_csv('finaltable?.csv')
+
+
+
+
+if __name__ == '__main__':
+  main()
